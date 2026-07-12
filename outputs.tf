60f0dae2350e5101b640bc3fef5d6249cb7e7e5f
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:01:01 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "cdn_frontdoor_rule_sets_id" {
+  description = "Map of id values across all cdn_frontdoor_rule_sets, keyed the same as var.cdn_frontdoor_rule_sets"
+  value       = { for k, v in azurerm_cdn_frontdoor_rule_set.cdn_frontdoor_rule_sets : k => v.id }
+}
 output "cdn_frontdoor_rule_sets_cdn_frontdoor_profile_id" {
   description = "Map of cdn_frontdoor_profile_id values across all cdn_frontdoor_rule_sets, keyed the same as var.cdn_frontdoor_rule_sets"
   value       = { for k, v in azurerm_cdn_frontdoor_rule_set.cdn_frontdoor_rule_sets : k => v.cdn_frontdoor_profile_id }
