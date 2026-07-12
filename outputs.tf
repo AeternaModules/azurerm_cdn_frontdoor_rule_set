0bacf4277e0cb2d6e73c46301cd21079684fb28b
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:23 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,9 @@
-output "cdn_frontdoor_rule_sets" {
-  description = "All cdn_frontdoor_rule_set resources"
-  value       = azurerm_cdn_frontdoor_rule_set.cdn_frontdoor_rule_sets
-}
 output "cdn_frontdoor_rule_sets_cdn_frontdoor_profile_id" {
-  description = "List of cdn_frontdoor_profile_id values across all cdn_frontdoor_rule_sets"
-  value       = [for k, v in azurerm_cdn_frontdoor_rule_set.cdn_frontdoor_rule_sets : v.cdn_frontdoor_profile_id]
+  description = "Map of cdn_frontdoor_profile_id values across all cdn_frontdoor_rule_sets, keyed the same as var.cdn_frontdoor_rule_sets"
+  value       = { for k, v in azurerm_cdn_frontdoor_rule_set.cdn_frontdoor_rule_sets : k => v.cdn_frontdoor_profile_id }
 }
 output "cdn_frontdoor_rule_sets_name" {
-  description = "List of name values across all cdn_frontdoor_rule_sets"
-  value       = [for k, v in azurerm_cdn_frontdoor_rule_set.cdn_frontdoor_rule_sets : v.name]
+  description = "Map of name values across all cdn_frontdoor_rule_sets, keyed the same as var.cdn_frontdoor_rule_sets"
+  value       = { for k, v in azurerm_cdn_frontdoor_rule_set.cdn_frontdoor_rule_sets : k => v.name }
 }
 
